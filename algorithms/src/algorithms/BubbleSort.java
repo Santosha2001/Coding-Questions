b6b9f91d0684c8b3520da8c6c3cd03c6b2f8f547
Santosha C <santosha5856@gmail.com>
2024-08-25 22:47:51 +0530

    algorithms

---
+++ algorithms/src/algorithms/BubbleSort.java
@@ -0,0 +1,29 @@
+package algorithms;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] array) {
+        int length = array.length;
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] array = {24, 8, 2, 98, 25, 67};
+        bubbleSort(array);
+
+        //output: 2 8 24 25 67 98
+    }
+}
---
+++ algorithms/src/algorithms/SelectionSort.java
@@ -0,0 +1,33 @@
+package algorithms;
+
+public class SelectionSort {
+
+    public static void selectionSort(int[] array) {
+        int length = array.length;
+
+        for (int i = 0; i < length - 1; i++) {
+
+            // Find the minimum element in unsorted array
+            int minIndex = i;
+            for (int j = i + 1; j < length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap the found minimum element with the first element
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+
+        for (int j : array) {
+            System.out.print(j + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] array = {98, 42, 67, 12, 8, 65};
+        selectionSort(array);
+    }
+}
